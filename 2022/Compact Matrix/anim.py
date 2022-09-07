8d080a9b0985c01627e45e818740b0374bb1ebf9
Gobinda Chandra <g.chandra.2501@icloud.com>
2022-09-07 11:22:08 +0530

    1

---
+++ 2022/Compact Matrix/anim.py
@@ -0,0 +1,33 @@
+############################################# by GOBINDA CHANDRA ###################################################
+
+                                    # VISIT : https://reanlea.com/ 
+                                    # YouTube : https://www.youtube.com/Reanlea/ 
+                                    # Twitter : https://twitter.com/Reanlea_ 
+                                    # Facebook : https://www.facebook.com/reanlea.ed/ 
+                                    # Telegram : https://t.me/reanlea/ 
+
+#####################################################################################################################
+
+import sys
+import copy
+
+
+from __future__ import annotations
+from cProfile import label
+import fractions
+from imp import create_dynamic
+from multiprocessing import context
+from multiprocessing.dummy import Value
+from numbers import Number
+from tkinter import Y, Label
+from imp import create_dynamic
+from turtle import degrees
+from manim import*
+from math import*
+from manim_fonts import*
+import numpy as np
+import random
+from sklearn.datasets import make_blobs
+from Common.reanlea_colors import*
+
+config.background_color= REANLEA_BACKGROUND_COLOR
