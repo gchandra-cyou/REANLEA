85536120f05f257c06b673b01b23926435db184b
Gobinda Chandra <g.chandra.2501@icloud.com>
2022-11-20 08:21:54 +0530

    1

---
+++ 2022/Compact Matrix/anim2.py
@@ -0,0 +1,77 @@
+############################################# by GOBINDA CHANDRA ###################################################
+
+                                    # VISIT : https://reanlea.com/ 
+                                    # YouTube : https://www.youtube.com/Reanlea/ 
+                                    # Twitter : https://twitter.com/Reanlea_ 
+                                    # Facebook : https://www.facebook.com/reanlea.ed/ 
+                                    # Telegram : https://t.me/reanlea/ 
+
+#####################################################################################################################
+
+from __future__ import annotations
+from ast import Return
+from cProfile import label
+from calendar import c
+from difflib import restore
+
+
+import fractions
+from imp import create_dynamic
+import math
+from multiprocessing import context
+from multiprocessing import dummy
+from multiprocessing.dummy import Value
+from numbers import Number
+import sre_compile
+from tkinter import Y, Label, font
+from imp import create_dynamic
+from tracemalloc import start
+from turtle import degrees, width
+from manim import*
+from math import*
+from manim_fonts import*
+import numpy as np
+import random
+from sklearn.datasets import make_blobs
+from reanlea_colors import*
+from func import*
+from PIL import Image
+#from func import EmojiImageMobject
+
+config.max_files_cached=500
+
+config.background_color= REANLEA_BACKGROUND_COLOR
+
+
+###################################################################################################################
+
+
+class Scene1(Scene):
+    def construct(self):
+        
+        # WATER MARK 
+
+        water_mark=ImageMobject("watermark.png").scale(0.1).move_to(5*LEFT+3*UP).set_opacity(0.15).set_z_index(-100)
+        self.add(water_mark)
+
+
+        self.wait(4)
+
+
+
+
+
+        # manim -pqh anim2.py Scene1
+
+        # manim -pql anim2.py Scene1
+
+        # manim -sqk anim2.py Scene1
+
+        # manim -sql anim2.py Scene1
+
+
+
+
+###################################################################################################################
+
+# cd "C:\Users\gchan\Desktop\REANLEA\2022\Compact Matrix" 
